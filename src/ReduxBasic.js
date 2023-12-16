30ef4af009a0e9dc732551346b0ad3b6bc1b72b1
podestal <luisrodriguezpodesta@gmail.com>
2023-12-16 10:07:45 -0500

    store and reducers created

---
+++ src/ReduxBasic.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit"
+
+
+
+
+
+
+
+const ReduxBasic = () => {
+    return (
+        <div>
+            Redux Basic ...
+        </div>
+    )
+}
+
+export default ReduxBasic
